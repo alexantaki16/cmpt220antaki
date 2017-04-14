ad6b5e6e403e9067f7ba7432c24ba47958516cd9
alexantaki16 <alexander.antaki1@marist.edu>
2017-04-14 17:04:43 -0400

    Add files via upload
    This my submission for the last question in Lab 6 and my UML diagram

---
+++ Labs/Lab 6/Question9_13.java
@@ -0,0 +1,101 @@
+/**
+ * file: Question9_13.java
+ * author: Alex Antaki
+ * course: CMPT 220
+ * assignment: Lab 6
+ * due date: April 18, 2017
+ * version: 1
+ *
+ *  This program will take in an array from the user and find the location of
+ * the largest number
+ */
+import java.util.Scanner;
+public class Question9_13{
+  public static void main(String[] args){
+
+    //Import Scanner
+    Scanner input = new Scanner(System.in);
+
+    //Prompt the user to enter how many rows and columns they want and store it
+    System.out.println("Enter the number of rows and columns in the array: ");
+    int rows = input.nextInt();
+    int cols = input.nextInt();
+
+    //Prompt the user to enter values based on the rows and columns they want
+    System.out.println("Please enter values for the " + rows + " rows and "
+        + cols + " columns");
+
+    //Create an array for the user
+    double[][] userArray = new double[rows][cols];
+
+    //Use nested for loops to store the input
+    for(int i = 0; i < userArray.length; i++){
+      for(int j = 0; j < userArray[i].length; j++){
+        userArray[i][j] = input.nextDouble();
+      }
+    }
+
+    //Create an instnace of the object and call if maxValLoc
+    Location maxValLoc = locateLargest(userArray);
+
+    //Display the location of the largest element in the array
+    System.out.println("The loactation of the largest element in the array is "
+        + " at ("  + maxValLoc.rows + ", " + maxValLoc.cols + ")");
+  }
+  /*
+   * Location
+   *
+   *  This class will take values from the user and determine where the largerst
+   * element in the array is.
+   *
+   */
+  public static class Location{
+
+    //Initalize all data fields
+    public int rows = 0;
+    public int cols = 0;
+    public double maxVal;
+
+    //Default constructor
+    Location(){
+
+    }
+
+    //Second construtor takes in the array being passed
+    Location(double[][] a){
+
+      //Sets default location for largest element
+      maxVal = a[0][0];
+
+      //Nested for loops are used to go through every index and check which is
+      // the largest
+      for(int i = 0; i < a.length; i++){
+        for(int j = 0; j < a[i].length; j++){
+          //Checks if the first input is greater than the next and checks every
+          // index.  If it is greater it will assign it to the maxVal.
+          if(a[i][j] > maxVal){
+            rows = i;
+            cols = j;
+            maxVal = a[i][j];
+          }
+        }
+      }
+    }
+  }
+  /*
+   * locateLargest
+   *
+   * This method will take the maxValue from the if statement above and assigne
+   * it to "answer"
+   *
+   * Parameters:
+   *   double[][] a: This array holds the user input and knows the highest
+   * element in the array
+   *
+   * Return value: The method returns the highest element in the array.
+  */
+    public static Location locateLargest(double[][] a){
+      Location answer = new Location(a);
+      return answer;
+    }
+ }
