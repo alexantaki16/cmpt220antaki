35f5e6f535a131317703f29a3bdb667d1ef43ea3
alexantaki16 <alexander.antaki1@marist.edu>
2017-05-02 21:22:59 -0400

    Update Question2.java

--- Labs/Revised Lab 2/Question2.java
+++ Labs/Revised Lab 2/Question2.java
@@ -18,40 +18,40 @@ public class Question2 {
     //Initialize grade to 0.
     double grade = 0;
 
- 		 System.out.println("Please enter your grades for your midterm, final, " +
- 		     "projects, and homework and labs as percentages: ");
+    System.out.println("Please enter your grades for your midterm, final, " 
+        +"projects, and homework and labs as percentages: ");
 
     //Have user input all four grades.
- 		 double midterm = input.nextDouble();
- 		 double final1 = input.nextDouble();
- 		 double projects = input.nextDouble();
- 		 double hwandlab = input.nextDouble();
+ 	  double midterm = input.nextDouble();
+ 		double final1 = input.nextDouble();
+ 	  double projects = input.nextDouble();
+ 	  double hwandlab = input.nextDouble();
 
     //Averages all inputed values
- 		 double average = (midterm + final1 + projects + hwandlab) / 4;
+ 	  double average = (midterm + final1 + projects + hwandlab) / 4;
 
-   //Calcualte letter grade for class
-   if(average >= 95)
-     System.out.print("Your final grade is: A");
-   else if(average >= 90)
-     System.out.print("Your final grade is: A-");
-   else if(average >= 87)
-     System.out.print("Your final grade is: B+");
-   else if(average >= 83)
-     System.out.print("Your final grade is: B");
-   else if(average >= 80)
-     System.out.print("Your final grade is : B-");
-   else if(average >= 77)
-     System.out.print("Your final grade is: C+");
-   else if(average >= 73)
-     System.out.print("Your final grade is: C");
-   else if(average >= 70)
-     System.out.print("Your final grade is: C-");
-   else if(average >= 65)
-     System.out.print("Your final grade is: D+");
-   else if(average >= 60)
-     System.out.print("Your final grade is: D");
-   else
-     System.out.print("Your final grade is: F");
+    //Calcualte letter grade for class
+    if(average >= 95)
+      System.out.print("Your final grade is: A");
+    else if(average >= 90)
+      System.out.print("Your final grade is: A-");
+    else if(average >= 87)
+      System.out.print("Your final grade is: B+");
+    else if(average >= 83)
+      System.out.print("Your final grade is: B");
+    else if(average >= 80)
+      System.out.print("Your final grade is : B-");
+    else if(average >= 77)
+      System.out.print("Your final grade is: C+");
+    else if(average >= 73)
+      System.out.print("Your final grade is: C");
+    else if(average >= 70)
+      System.out.print("Your final grade is: C-");
+    else if(average >= 65)
+      System.out.print("Your final grade is: D+");
+    else if(average >= 60)
+      System.out.print("Your final grade is: D");
+    else
+      System.out.print("Your final grade is: F");
   }
 }
